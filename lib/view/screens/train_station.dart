cca67c59f8f469de4bf4cef9ab655ee8bfb19b8e
AhmadAbed98nnnn <ahmadabed.7651@gmail.com>
2024-09-29 02:41:04 +0300

    update

--- lib/view/screens/train_station.dart
+++ lib/view/screens/train_station.dart
@@ -6,6 +6,7 @@ import 'package:zahra/custom/zahra_container.dart';
 import 'package:zahra/custom/zahra_texts.dart';
 import 'package:zahra/view/drawerwidgets/homescreen_drawerwidget.dart';
 import 'package:zahra/view/screens/home_screen.dart';
+import 'package:zahra/custom/zahra_colors.dart';
 
 class TrainStation extends StatelessWidget {
   const TrainStation({super.key});
@@ -43,12 +44,69 @@ class TrainStation extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     zahraboldtexts(': العنوان'),
                     SizedBox(
                       height: mediaqueryheight(
-                        0.015,
+                        0.025,
                         context,
+                      ),
+                    ),
+                    getnormaltexts('اضافة العنوان بالتفصيل'),
+                    SizedBox(
+                      height: mediaqueryheight(
+                        0.025,
+                        context,
+                      ),
+                    ),
+                    zahraboldtexts('مواعيد القطار'),
+                    SizedBox(
+                      height: mediaqueryheight(
+                        0.025,
+                        context,
+                      ),
+                    ),
+                    getnormaltexts('من الساعة 6  صباحا و حتى الساعة 11 مساء'),
+                    SizedBox(
+                      height: mediaqueryheight(
+                        0.025,
+                        context,
+                      ),
+                    ),
+                    zahraboldtexts('-: ارقام التليفون'),
+                    SizedBox(
+                      height: mediaqueryheight(
+                        0.025,
+                        context,
+                      ),
+                    ),
+                    getnormaltexts('010000000000 -024679458 - 0254765475'),
+                    SizedBox(
+                      height: mediaqueryheight(
+                        0.025,
+                        context,
+                      ),
+                    ),
+                    Text(
+                      'الموقع على الخريطة',
+                      style: GoogleFonts.cairo(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: bgButtonColor,
+                      ),
+                    ),
+                    SizedBox(
+                      height: mediaqueryheight(
+                        0.025,
+                        context,
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: Image.asset(
+                        'assets/trainstationmap.png',
+                        fit: BoxFit.fill,
                       ),
                     ),
                   ],
